16e166eaf7a2bd5b9c9747d677cee7cdc32f0d4b
Lange <langescomputerscience@gmail.com>
2025-04-23 14:15:31 -0400

    Cop Car LED flashing - Stable

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -1,24 +1,5 @@
-/*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/basics/Fade/
-*/
-
-// Define the LED pins
-const int led1 = 9;  // PWM pin
-const int led2 = 10; // PWM pin
-
-// Fading delay time (smaller = faster)
-int fadeDelay = 10;
+const int led1 = 9;  // Left light
+const int led2 = 10; // Right light
 
 void setup() {
   pinMode(led1, OUTPUT);
@@ -26,31 +7,25 @@ void setup() {
 }
 
 void loop() {
-  // Fade in LED 1
-  for (int brightness = 0; brightness <= 255; brightness++) {
-    analogWrite(led1, brightness);
-    delay(fadeDelay);
+  // Flash left LED 3 times
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    delay(100);
+    digitalWrite(led1, LOW);
+    delay(100);
   }
 
-  // Fade out LED 1
-  for (int brightness = 255; brightness >= 0; brightness--) {
-    analogWrite(led1, brightness);
-    delay(fadeDelay);
+  delay(200); // Short pause
+
+  // Flash right LED 3 times
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(led2, HIGH);
+    digitalWrite(led1, LOW);
+    delay(100);
+    digitalWrite(led2, LOW);
+    delay(100);
   }
 
-  delay(500); // Small pause before starting LED 2
-
-  // Fade in LED 2
-  for (int brightness = 0; brightness <= 255; brightness++) {
-    analogWrite(led2, brightness);
-    delay(fadeDelay);
-  }
-
-  // Fade out LED 2
-  for (int brightness = 255; brightness >= 0; brightness--) {
-    analogWrite(led2, brightness);
-    delay(fadeDelay);
-  }
-
-  delay(500); // Pause before repeating
+  delay(300); // Slightly longer pause before repeating
 }
